040dc51276721b1f6c3b39412d92f090a3d241da
Nimesh Ariyarathne <80301441+Nimesh55@users.noreply.github.com>
2023-12-28 06:34:00 +0530

    Update App.jsx

--- Frontend/online-quiz-client/src/App.jsx
+++ Frontend/online-quiz-client/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import AddQuestion from "../components/question/AddQuestion";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/layout/NavBar";
+import Admin from "./components/Admin";
+import QuizStepper from "./components/quiz/QuizStepper";
+import Quiz from "./components/quiz/Quiz";
+import QuizResult from "./components/quiz/QuizResult";
+import GetAllQuiz from "./components/quiz/GetAllQuiz";
+import AddQuestion from "./components/question/AddQuestion";
+import UpdateQuestion from "./components/question/UpdateQuestion";
 
 function App() {
   return (
-    <>
-      <AddQuestion />
-    </>
+    <main className="container mt-5 mb-5">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz-stepper" element={<QuizStepper />} />
+          <Route path="/take-quiz" element={<Quiz />} />
+          <Route path="/admin" element={<Admin />} />
+
+          <Route path="/create-quiz" element={<AddQuestion />} />
+          <Route path="/update-quiz/:id" element={<UpdateQuestion />} />
+          <Route path="/all-quizzes" element={<GetAllQuiz />} />
+          <Route path="/quiz-result" element={<QuizResult />} />
+        </Routes>
+      </Router>
+    </main>
   );
 }
 
